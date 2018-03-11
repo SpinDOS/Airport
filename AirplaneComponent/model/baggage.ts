38f9f0205f79eba0fe6cb741e5492dbd0776a7a8
Alexander Platonov <alexanderplat757@gmail.com>
2018-03-11 15:55:11 +0300

    Add Baggage reference to Passenger

--- AirplaneComponent/model/baggage.ts
+++ AirplaneComponent/model/baggage.ts
@@ -3,6 +3,6 @@ import { IPassenger } from "./passenger";
 
 export interface IBaggage {
   id: Guid,
-  owner?: IPassenger,
   weight: number,
+  owner?: IPassenger,
 }
--- AirplaneComponent/model/passenger.ts
+++ AirplaneComponent/model/passenger.ts
@@ -1,6 +1,8 @@
 import { Guid } from "guid-typescript";
+import { IBaggage } from "./baggage";
 
 export interface IPassenger {
   id: Guid,
   name?: string,
+  baggage?: IBaggage
 }
