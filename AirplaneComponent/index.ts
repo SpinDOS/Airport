aea31610c8ae4e7d8f5bb8a0572ca9bd235cf254
Alexander Platonov <alliotalp@yandex.ru>
2018-03-15 01:33:37 +0300

    Remove test code

--- AirplaneComponent/index.ts
+++ AirplaneComponent/index.ts
@@ -1,21 +1,7 @@
-import 'reflect-metadata';
-import * as logger from './utils/logger';
-import * as mqListen from './mq/mqListen';
-import { createAirplane } from './mq/airplaneCreator';
+import "reflect-metadata";
+import * as logger from "./utils/logger";
+import * as mqListen from "./mq/mqListen";
+import { Flight } from "./model/validation/Flight";
 
 mqListen.start();
-logger.log('Working');
-
-
-let json = {
-  landingFlight: {
-    id: '46536023-92ca-4fc2-8e96-4e26faafadee',
-    code: 'land'
-  },
-
-  departureFlight: {
-    //id: '46536023-92ca-4fc2-8e96-4e26faafadee',
-    code: 'dep'
-  },
-}
-createAirplane(json);
+logger.log("Working");
