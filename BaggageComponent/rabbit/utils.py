5cb3ffb06bc6928ae0163731b77c9990646d3fde
dshepelev15 <d-shepelev@list.ru>
2018-03-22 23:14:25 +0300

    Add thread for consumption reply_to queue

--- BaggageComponent/rabbit/utils.py
+++ BaggageComponent/rabbit/utils.py
@@ -1,6 +1,16 @@
+import pika
 from datetime import datetime
 
 FILE = 'log.txt'
+
+
+def get_channel():
+    # credentials = pika.PlainCredentials('user', 'password')
+    # parameters = pika.ConnectionParameters('IP here', 5672, '/', credentials)
+    # connection = pika.BlockingConnection(parameters)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    return channel
 
 
 def log_message(message):
@@ -12,4 +22,4 @@ def log_message(message):
 def validate_response(message, expected_fields):
     for field in expected_fields:
         if field not in message:
-            raise Exception("Incorrect message format")
+            raise Exception("Incorrect message format fields: {}, message {}".format(expected_fields, message))
