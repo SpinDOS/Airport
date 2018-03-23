545b21d74f2178bb0f0aa4ad26553abda04fbe3f
Alexander Platonov <alliotalp@yandex.ru>
2018-03-24 01:00:46 +0300

    Fix spacing

--- AirplaneComponent/index.ts
+++ AirplaneComponent/index.ts
@@ -1,4 +1,4 @@
-import { start as startMQListen} from "./mq/mq";
+import { start as startMQListen } from "./mq/mq";
 import { start as startHttpServer } from "./webapi/httpServer";
 
 startHttpServer();
