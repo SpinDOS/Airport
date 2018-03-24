d36ab5930d61e250f1a8be4248f4bb639e7c7aba
Alexander Platonov <alliotalp@yandex.ru>
2018-03-24 03:14:47 +0300

    Create passengerAPI module

--- AirplaneComponent/webapi/passengersAPI.ts
+++ AirplaneComponent/webapi/passengersAPI.ts
@@ -1,5 +1,16 @@
+import { Guid } from "guid-typescript";
 import * as rp from "request-promise";
+
+import { strToPOCO } from "../utils/utils";
 import { onApiError } from "../errors/connectionError";
+import { ValidationError } from "../errors/validationError";
+
+import { IPassenger } from "../model/passenger";
+import { IBaggage } from "../model/baggage";
+import { IResponsePassenger, validatePassenger,
+  IPassBagCreateRes, validatePasBagCreateResponse } from "../model/validation/passBagCreateRes";
+
+
 
 export const passengersUrl: string = "http://quantum0.pythonanywhere.com/";
 
@@ -18,4 +29,40 @@ export async function post(url: string, body: object): Promise<any> {
     body: JSON.stringify(body)
   })
   .catch(e => onApiError("Passenger API", e));
+}
+
+export function parseArrayOfPassengers(data: string): IResponsePassenger[] {
+    let poco: object = strToPOCO(data);
+    if (!(poco instanceof Array)) {
+      throw new ValidationError("Passenger API response error: JSON array of passengers expected. Got: " + data);
+    }
+    return poco.map(validatePassenger);
+}
+
+
+export class PasAndBag {
+  constructor(public readonly passengers: IPassenger[],
+    public readonly baggage: IBaggage[]) { }
+}
+
+export function parseGenerateResponse(data: string): PasAndBag {
+  let poco: object = strToPOCO(data);
+  let response: IPassBagCreateRes = validatePasBagCreateResponse(poco);
+
+  let passengers: IPassenger[] = response.passengers.map(p => {
+    return {
+      id: p.id,
+      name: p.first_name,
+      baggage: p.luggage !== "None"? p.luggage: undefined,
+    };
+  });
+
+  let baggages: IBaggage[] =
+    response.passengers.filter(p => p.luggage !== "None").map(p => p.luggage as Guid)
+    .concat(response.service_luggage)
+    .map(guid => {
+      return { id: guid };
+    });
+
+  return new PasAndBag(passengers, baggages);
 }
