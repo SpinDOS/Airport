141950c932da68d63359dd3c88b082a80701224b
Nikitikon <Nikitikon@gmail.com>
2018-03-27 11:26:14 +0300

    Ready For Test

--- Followme/followme.py
+++ Followme/followme.py
@@ -37,7 +37,7 @@ except:
     from queue import Queue
 
 
-
+URL = ""
 
 def sendMovement(carId, fromId, toId, status):
     connection = pika.BlockingConnection(pika.URLParameters(
@@ -52,6 +52,33 @@ def sendMovement(carId, fromId, toId, status):
         "from":f"{fromId}",
         "to":f"{toId}",
         "status": f"{status}"
+    })
+    channel.basic_publish(exchange='',
+                          routing_key='gtc.gate',
+                          body=message,
+                          properties=pika.BasicProperties(
+                              content_type="json",
+                              correlation_id=f"{carId}",
+                              reply_to="FMMQ",
+                              delivery_mode = 2
+                          ))
+    print(" [x] Sent %r" % (json.loads(message)))
+
+    connection.close()
+
+
+def sendAccept(carId, airplane_id, parking_id):
+    connection = pika.BlockingConnection(pika.URLParameters(
+        'amqp://aazhpoyi:wl3G3Fu_s88DNK0Fr0N9XxsUBxmlzUcK@duckbill.rmq.cloudamqp.com/aazhpoyi'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='gtc.gate', durable=True)
+
+    message = json.dumps({
+        "service":"follow_me",
+        "request":"accept",
+        "airplane_id":f"{airplane_id}",
+        "parking_id":f"{parking_id}"
     })
     channel.basic_publish(exchange='',
                           routing_key='gtc.gate',
@@ -90,7 +117,41 @@ def sendMaintain(carId, aircraftId):
 
     connection.close()
 
+def sendVisualize(currentLocation, tempLocation, carid, airplane_id):
+    connection = pika.BlockingConnection(pika.URLParameters(
+        'amqp://aazhpoyi:wl3G3Fu_s88DNK0Fr0N9XxsUBxmlzUcK@duckbill.rmq.cloudamqp.com/aazhpoyi'))
+    channel = connection.channel()
 
+    channel.queue_declare(queue='visualizer', durable=True)
+
+    if airplane_id == 0:
+        message = json.dumps({
+            "Type": "movement",
+            "From": f"{currentLocation}",
+            "To": f"{tempLocation}",
+            "Transport": f"Fuel|{carid}",
+            "Duration": f"1000",
+        })
+    else:
+        message = json.dumps({
+            "Type": "movement",
+            "From": f"{currentLocation}",
+            "To": f"{tempLocation}",
+            "Transport": f"Fuel|{carid}",
+            "Duration": f"1000",
+            'Aircraft': f'{airplane_id}'
+        })
+    channel.basic_publish(exchange='',
+                          routing_key='visualizer',
+                          body=message,
+                          properties=pika.BasicProperties(
+                              content_type="json",
+                              correlation_id=f"{carid}",
+                              delivery_mode=2
+                          ))
+    print(" [x] Sent %r" % (json.loads(message)))
+
+    connection.close()
 
 
 class Followme(threading.Thread):
@@ -146,7 +207,7 @@ class Followme(threading.Thread):
                                 if self.carDict[car] and self.stripDict[strip]:
                                     self.carDict[car] = False
                                     self.stripDict[strip] = False
-                                    t = threading.Thread(target=self.takeoff, args=(self.queueTakeoff.get(),))
+                                    t = threading.Thread(target=self.takeoff, args=(self.queueTakeoff.get(),strip,car,))
                                     t.start()
                                     self.queueTakeoff.task_done()
 
@@ -161,7 +222,7 @@ class Followme(threading.Thread):
                                             self.carDict[car] = False
                                             self.stripDict[strip] = False
                                             self.parkingDict[parking] = False
-                                            t = threading.Thread(target=self.loadboard, args=(self.queueLoad.get(),))
+                                            t = threading.Thread(target=self.loadboard, args=(self.queueLoad.get(),parking,strip,car,))
                                             t.start()
                                             self.queueLoad.task_done()
             if not(self.queueAnswer.empty()):
@@ -178,12 +239,149 @@ class Followme(threading.Thread):
 
 
 
-    def takeoff(self, massege, ):
-        print(massege)
+    def takeoff(self, massege, strip, car):
+        if massege['request'] != 'service':
+            print("Чет пошло не так")
 
-    def loadboard(self, massege):
-        print(massege)
+        airplane_id = None
+        parking_id = None
 
+        try:
+            airplane_id = massege['airplane_id']
+            parking_id = massege['parking_id']
+        except:
+            print("Неверный формат")
+            exit(1)
+
+        currentPos = "FollowMeGarage|1"
+        nextPos = currentPos
+        finishPos = parking_id + "FollowMeBack"
+
+        self.walk(currentPos=currentPos, nextPos=nextPos, finishPos=finishPos, carid=car, airplane_id=0)
+        self.startToStrip(carid=car, airplane_id=airplane_id)
+
+        currentPos = finishPos
+        nextPos = currentPos
+        finishPos = strip + "AddFollowMe"
+
+        with self.parkingLock:
+            self.parkingDict[parking_id] = True
+
+        self.walk(currentPos=currentPos, nextPos=nextPos, finishPos=finishPos, carid=car, airplane_id=airplane_id)
+
+        self.endToStrip(stripid=strip, airplane_id=airplane_id)
+
+        sendMaintain(carId=car, aircraftId=airplane_id)
+
+        currentPos = finishPos
+        nextPos = currentPos
+        finishPos = "FollowMeGarage|1"
+
+        with self.stripLock:
+            self.stripDict[strip] = True
+        self.walk(currentPos=currentPos, nextPos=nextPos, finishPos=finishPos, carid=car, airplane_id=0)
+        with self.carLock:
+            self.carDict[car] = True
+
+
+
+    def loadboard(self, massege, parking, strip, car):
+        if massege['request'] != 'landing':
+            print("Чет пошло не так")
+        airplane_id = None
+        try:
+            airplane_id = massege['aircraftId']
+        except:
+            print("Неверный формат")
+            exit(1)
+        self.sendStrip(airplane_id, strip, car)
+        currentPos = "FollowMeGarage|1"
+        nextPos = currentPos
+        finishPos = strip+"FollowMe"
+
+        self.walk(currentPos=currentPos, nextPos=nextPos, finishPos=finishPos, carid=car, airplane_id=0)
+
+        landing = False
+        while not(landing):
+            with self.answerLock:
+                for answer in self.answerDict:
+                    if answer['request'] == 'landingcomp':
+                        landing = True
+
+        self.startToParking(car, airplane_id)
+        currentPos = finishPos
+        nextPos = currentPos
+        finishPos = parking + "FollowMe"
+
+        with self.stripLock:
+            self.stripDict[strip] = True
+        self.walk(currentPos=currentPos, nextPos=nextPos, finishPos=finishPos, carid=car, airplane_id=airplane_id)
+        self.EndToParking(parkingid=parking, airplane_id=airplane_id)
+
+        sendAccept(carId=car, airplane_id=airplane_id, parking_id=parking)
+
+        currentPos = finishPos
+        nextPos = currentPos
+        finishPos = "FollowMeGarage|1"
+        self.walk(currentPos=currentPos, nextPos=nextPos, finishPos=finishPos, carid=car, airplane_id=0)
+        with self.carLock:
+            self.carDict[car] = True
+
+
+    def walk(self, currentPos, nextPos, finishPos, carid, airplane_id):
+        while currentPos != finishPos:
+            sendMovement(carid, currentPos, finishPos, "need")
+
+            while nextPos == currentPos:
+                with self.answerLock:
+                    for answer in self.answerDict:
+                        if answer['request'] == 'movement':
+                            nextPos = answer['to']
+            sendVisualize(currentLocation=currentPos, tempLocation=nextPos, carid=carid, airplane_id=airplane_id)
+            sendMovement(carid, currentPos, finishPos, "done")
+            nextPos = currentPos
+
+    def startToParking(self, carid,airplane_id):
+        data = {'carId': carid}
+        post_url = URL + '/' + airplane_id + '/followMe/startToParking'
+        r = requests.post(url=post_url, data=data)
+        while r.status_code != 200:
+            if r.status_code == 404:
+                print("Самолетик пропал: ", airplane_id)
+                exit(1)
+            r = requests.post(url=post_url, data=data)
+
+
+    def EndToParking(self, parkingid,airplane_id):
+        data = {'parkingId': parkingid}
+        post_url = URL + '/' + airplane_id + '/followMe/EndToParking'
+        r = requests.post(url=post_url, data=data)
+        while r.status_code != 200:
+            if r.status_code == 404:
+                print("Самолетик пропал: ", airplane_id)
+                exit(1)
+            r = requests.post(url=post_url, data=data)
+
+
+    def startToStrip(self, carid,airplane_id):
+        data = {'carId': carid}
+        post_url = URL + '/' + airplane_id + '/followMe/startToStrip'
+        r = requests.post(url=post_url, data=data)
+        while r.status_code != 200:
+            if r.status_code == 404:
+                print("Самолетик пропал: ", airplane_id)
+                exit(1)
+            r = requests.post(url=post_url, data=data)
+
+    def endToStrip(self, stripid,airplane_id):
+        data = {'stripId': stripid}
+        post_url = URL + '/' + airplane_id + '/followMe/endToStrip'
+        r = requests.post(url=post_url, data=data)
+        while r.status_code != 200:
+            if r.status_code == 404:
+                print("Самолетик пропал: ", airplane_id)
+                exit(1)
+            r = requests.post(url=post_url, data=data)
 
     def sendStrip(self, aircraftid, stripid, carid):
         connection = pika.BlockingConnection(pika.URLParameters(
