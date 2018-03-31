15e0ceb672aebb6432098132c85e0e21738d9e34
dshepelev15 <d-shepelev@list.ru>
2018-03-31 20:42:45 +0300

    Add bus component

---
+++ BusComponent/config.py
@@ -0,0 +1,7 @@
+AIRPLANE_API = 'http://192.168.0.41:8081'
+AIRPLANE_QUEUE = 'Airplane'
+PASSENGER_API = 'http://quantum0.pythonanywhere.com'
+VISUALIZER_QUEUE = 'visualizer'
+UND_ROUTING_KEY = 'gtc.gate'
+UND_EXCHANGE = 'amqp.gtc'
+BUS_QUEUE = 'Bus'
